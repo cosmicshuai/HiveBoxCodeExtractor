1a9bc5e797ea9a13b367e98d3b26ec0e0e03671b
Shuai Wang <shuai_uf@outlook.com>
2021-06-28 16:53:52 +0800

    change to hivecode

---
+++ HiveBoxCodeExtractor/Controllers/HiveBoxCodeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace HiveBoxCodeExtractor.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HiveBoxCodeController : ControllerBase
+    {
+        private readonly ILogger<HiveBoxCodeController> _logger;
+
+        public HiveBoxCodeController(ILogger<HiveBoxCodeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            var codeEvent = new HiveCodeEvent
+            {
+                Date = DateTime.Now,
+                Location = "MidSea Area 7 Building 16",
+                BoxNumber = 3,
+                openCode = "23114533",
+                vendor = "SF Express"
+            };
+
+            return $"You have a new Packege, @{codeEvent.Location}, box: {codeEvent.BoxNumber}, code: {codeEvent.openCode}";
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "";
+        }
+    }
+}
---
+++ HiveBoxCodeExtractor/HiveCodeEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HiveBoxCodeExtractor
+{
+    public class HiveCodeEvent
+    {
+        public DateTime Date { get; set; }
+
+        public string Location { get; set; }
+
+        public int BoxNumber { get; set; }
+
+        public string openCode { get; set; }
+
+        public string vendor { get; set; }
+    }
+}
