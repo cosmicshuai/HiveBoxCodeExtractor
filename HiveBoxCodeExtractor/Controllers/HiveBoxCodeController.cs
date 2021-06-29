b2985f6cd64283dcf88a1c5bfb7521aafcf737d8
Shuai Wang <shuai_uf@outlook.com>
2021-06-29 19:46:40 +0800

    update regex rule

--- HiveBoxCodeExtractor/Controllers/HiveBoxCodeController.cs
+++ HiveBoxCodeExtractor/Controllers/HiveBoxCodeController.cs
@@ -22,7 +22,8 @@ namespace HiveBoxCodeExtractor.Controllers
         public string GetBoxCode([FromQuery]string content)
         {
             //【丰巢】凭取件码63610565至海悦花园七区16幢架空层3号丰巢取中通快递包裹。有疑问联系快递员13732531641
-            Regex fcRegex = new(@"^【丰巢】.*凭取件码([0-9]{8})至(.*)丰巢取(.{4})包裹。.*");
+            //【丰巢】凭取件码29699459至海悦花园七区16幢架空层2号丰巢柜取中通快递包裹。有疑问联系快递员13732531641
+            Regex fcRegex = new(@"^【丰巢】.*凭取件码([0-9]{8})至(.*)丰巢.*包裹。.*");
             if (fcRegex.IsMatch(content))
             {
                 var matched = fcRegex.Match(content);
@@ -32,7 +33,7 @@ namespace HiveBoxCodeExtractor.Controllers
             }
 
             //【菜鸟驿站】您的中通包裹已到苏州海悦花园七区物业店，请21:00前凭3-1-2009取件，详询13451534429
-            Regex cnRegex = new(@"^【菜鸟驿站】.*已到(.*)，请.*凭(\d-\d-\d{4})取件，.*");
+            Regex cnRegex = new(@"^【菜鸟驿站】.*到(.*)，请.*凭(\d-\d-\d{4})取件，.*");
             if (cnRegex.IsMatch(content))
             {
                 var matched = cnRegex.Match(content);
