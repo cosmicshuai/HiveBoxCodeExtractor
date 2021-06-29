ac3c209b2997cbca8d732013f99513ebd8e1f7c3
Shuai Wang <shuai_uf@outlook.com>
2021-06-29 22:45:25 +0800

    add cache

--- HiveBoxCodeExtractor/Controllers/HiveBoxCodeController.cs
+++ HiveBoxCodeExtractor/Controllers/HiveBoxCodeController.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+using LruCacheNet
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -12,6 +12,8 @@ namespace HiveBoxCodeExtractor.Controllers
     {
         private readonly ILogger<HiveBoxCodeController> _logger;
 
+        private LruCache<string, int> _cache = new LruCache<string, int>(10000);
+
         public HiveBoxCodeController(ILogger<HiveBoxCodeController> logger)
         {
             _logger = logger;
@@ -21,6 +23,11 @@ namespace HiveBoxCodeExtractor.Controllers
         [HttpGet]
         public string GetBoxCode([FromQuery]string content)
         {
+            if (_cache.ContainsKey(content))
+            {
+                return "Not Valid";
+            }
+
             //【丰巢】凭取件码63610565至海悦花园七区16幢架空层3号丰巢取中通快递包裹。有疑问联系快递员13732531641
             //【丰巢】凭取件码29699459至海悦花园七区16幢架空层2号丰巢柜取中通快递包裹。有疑问联系快递员13732531641
             Regex fcRegex = new(@"^【丰巢】.*凭取件码([0-9]{8})至(.*)丰巢.*包裹。.*");
